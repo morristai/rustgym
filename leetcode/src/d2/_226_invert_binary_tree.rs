be9f2ef11185c0b8bf0a535d032653fb5a22d86d
Morris Tai <morristai@apache.org>
2025-03-09 00:28:40 -0500

    feat: add 226_invert_binary_tree Iterative
    Signed-off-by: Morris Tai <morristai@apache.org>

--- leetcode/src/d2/_226_invert_binary_tree.rs
+++ leetcode/src/d2/_226_invert_binary_tree.rs
@@ -1,15 +1,37 @@
 struct Solution;
+
+use std::mem;
 use rustgym_util::*;
 
 impl Solution {
     fn invert_tree(root: TreeLink) -> TreeLink {
-        if let Some(node) = &root {
-            let mut node = node.borrow_mut();
-            let left = node.left.take();
-            let right = node.right.take();
-            node.right = Self::invert_tree(left);
-            node.left = Self::invert_tree(right);
+        // ============ Recursive ============
+        // if let Some(node) = &root {
+        //     let mut node = node.borrow_mut();
+        //     let left = node.left.take();
+        //     let right = node.right.take();
+        //     node.right = Self::invert_tree(left);
+        //     node.left = Self::invert_tree(right);
+        // }
+        // root
+
+        // ============ Iterative ============
+        let mut stack = vec![];
+        stack.push(root.clone());
+
+        while !stack.is_empty() {
+            if let Some(Some(node)) = stack.pop() {
+                let mut borrowed = node.borrow_mut();
+                let borrowed = &mut *borrowed;
+                // NOTE: You can't pre clone cause the swap will only swap the Rc pointer not the value.
+                // let mut left = borrowed.left.clone();
+                // let mut right = borrowed.right.clone();
+                mem::swap(&mut borrowed.left, &mut borrowed.right);
+                stack.push(borrowed.left.clone());
+                stack.push(borrowed.right.clone());
+            }
         }
+
         root
     }
 }
